4adc0a3b723343b63007721dc75b7b18efad0cc2
Mateus Junges <mateusf.junges@gmail.com>
2018-09-26 22:07:38 -0300

    Update controle-servo-motor.ino

--- controle-servo-motor/controle-servo-motor.ino
+++ controle-servo-motor/controle-servo-motor.ino
@@ -1,10 +1,10 @@
 #include <Servo.h>
 
-#define pinoServo0 8
-#define pinoServo1 7
-#define pinoServo2 4
+#define numero_servos 3
 
-Servo servo0, servo1, servo2;  // Criacao de um objeto servo para controle
+int pino_servos[3] = {8, 7, 4};
+
+Servo servos[3];  // Criacao de um objeto servo para controle
 
 int pinoDIG = 12; // Pino digital para controle de um dos servos
 
@@ -16,13 +16,18 @@ int valorServo[3] = {41, 31, 21};
 int incremento[3] = {1, 1, 1};
 
 void setup() {
-  pinMode(pinoServo0, OUTPUT);
+  for(int n = 0; n < numero_servos; n++){
+  	pinMode(pino_servos[n], OUTPUT);
+  }
+  //pinMode(pinoServo0, OUTPUT);
   pinMode(pinoDIG, OUTPUT);
-  pinMode(pinoServo1, OUTPUT);
-  pinMode(pinoServo2, OUTPUT);
-  servo0.attach(pinoServo0);  // Relaciona o pino com o Servo
-  servo1.attach(pinoServo1);  // Relaciona o pino com o Servo
-  servo2.attach(pinoServo2);  // Relaciona o pino com o Servo
+  //pinMode(pinoServo1, OUTPUT);
+  //pinMode(pinoServo2, OUTPUT);
+ 
+  //Relaciona os servos com seus devidos pinos:
+  for(int n = 0; n < numero_servos; n++){
+  	servos[n].attach(pino_servos[n]);
+  }
 }
 
 void loop() {
@@ -38,9 +43,9 @@ void loop() {
   int valServo1 = map(leitura, 0, 1023, 0, 2000);// Valor para movimentação do servo1 (0 .. 2000 micro segundos)
   servoPulse(pinoDIG, valServo1); // Move servo2 para posicao desejada  
   
-  movServo(0, pinoServo0); 
-  movServo(1, pinoServo1);
-  movServo(2, pinoServo2);
+  movServo(0, pino_servos[0]); 
+  movServo(1, pino_servos[1]);
+  movServo(2, pino_servos[2]);
 }
  
 void servoPulse (int servo, int valor) {
@@ -59,25 +64,10 @@ void servoPulse (int servo, int valor) {
 void movServo(int servo, int myServo){
   //Se chegou no valor máximo ou mínimo, eu altero o sinal do incremento
   if(maxServo[servo] == valorServo[servo] || minServo[servo] == valorServo[servo]){
-    incremento[servo] = incremento[servo] * -1;
+  	incremento[servo] = incremento[servo] * -1;
   }
   
   valorServo[servo] += incremento[servo]; //valor usado no servo.write
-  
-  /*
-  * Verifica qual é o servo onde escrever:
-  */
-  switch (servo){
-    case 0: 
-      servo0.write(valorServo[servo]);
-      break;
-    case 1: 
-      servo2.write(valorServo[servo]);
-      break;
-    case 2: 
-      servo1.write(valorServo[servo]);
-      break;
-  }
-    
+  servos[servo].write(valorServo[servo]);
+  	
 }
-
